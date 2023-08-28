d75259950e94af9a1c11f60d4c611300752b58c5
Herve Allanic <herve.allanic@isep.fr>
2023-08-28 10:09:01 +0200

    Commit initial

---
+++ juste_prix.py
@@ -0,0 +1,18 @@
+# Tirage d'un prix (entier) au hasard entre 1 et 10
+import random
+
+random.randint(1,10)
+
+# Essai d'un prix
+input("essai : ")
+
+# Message "BRAVO" si le prix est trouvé
+
+
+# Message "PAS ASSEZ" si le prix est trop bas
+
+# Message "TROP ELEVE" si le prix est trop haut
+
+# Message "PERDU" au bout de 5 essais
+
+# Fin au bout de 5 esssais ou si le prix est trouvé
